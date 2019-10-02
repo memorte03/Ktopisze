90bba54443fe75a6eca86c491acc8d326759820a
Dawid Harat <david03h@wp.pl>
2019-10-02 21:24:21 +0200

    up

---
+++ routes/router.js
@@ -0,0 +1,12 @@
+var react = require('react');
+var path = require('path');
+var fs = require('fs');
+var reactDOMServer = require('react-dom/server');
+var StaticRouter = require('react-router-dom').StaticRouter;
+
+module.exports = function(app) {
+
+	app.get('/*', (request, response) => {
+		
+	});
+};
